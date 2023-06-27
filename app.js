a6580ab2e913ed8637c19a109dfeb302aec02a9a
Evanslodoctor <evanschaun@gmail.com>
2023-06-27 16:17:57 +0300

    Saving

--- app.js
+++ app.js
@@ -11,3 +11,21 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+// Mail to javascript code
+
+document.getElementById('sendEmailLink').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent link navigation
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const subject = document.getElementById('subject').value;
+    const message = document.getElementById('message').value;
+
+    const mailtoLink = `mailto:evanschaun@gmail.com.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}%0D%0A%0D%0AFrom:%0D%0A${encodeURIComponent(name)}%0D%0A${encodeURIComponent(email)}`;
+
+    window.location.href = mailtoLink;
+
+    // Optionally, you can reset the form after submission
+    document.getElementById('contactForm').reset();
+  });
